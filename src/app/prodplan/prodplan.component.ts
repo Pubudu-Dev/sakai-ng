53a6a90b5c224a4696848325e60d5a9a646320ec
Pubudu Ekanayaka <pubuduekanayake8@gmail.com>
2023-08-16 08:28:51 +0530

    added some changes

--- src/app/prodplan/prodplan.component.ts
+++ src/app/prodplan/prodplan.component.ts
@@ -1,21 +1,80 @@
 
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { GanttItem } from '@worktile/gantt';
+import TimelinesChart from 'timelines-chart';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+
 
 
 
 declare var Gantt: any; // Import Gantt from the global scope (added by including frappe-gantt.min.js in scripts)
 @Component({
   selector: 'app-prodplan',
-  templateUrl: './prodplan.component.html',
+  template: '<div #chartContainer></div>',
   styleUrls: ['./prodplan.component.scss']
 })
-export class ProdplanComponent {
+export class ProdplanComponent implements AfterViewInit{
+   myChart = TimelinesChart();
 
-  items: GanttItem[] = [
-    { id: '000000', title: 'Task 0', start: 1627729997, end: 1628421197 },
-    { id: '000001', title: 'Task 1', start: 1617361997, end: 1625483597 }
-  ];
+  @ViewChild('chartContainer') chartContainer!: ElementRef;
+
+  ngAfterViewInit() {
+    const myData = [
+      {
+        group: "Group 1",
+        data: [
+          {
+            label: "Data Point 1",
+            data: [
+              {
+                timeRange: [new Date('2023-07-01'), new Date('2023-07-05')],
+                val: 10
+              },
+              {
+                timeRange: [new Date('2023-07-10'), new Date('2023-07-15')],
+                val: 15
+              },
+              // Add more data points here
+            ]
+          },
+          {
+            label: "Data Point 2",
+            data: [
+              {
+                timeRange: [new Date('2023-07-05'), new Date('2023-07-08')],
+                val: 8
+              },
+              // Add more data points for 'Data Point 2'
+            ]
+          },
+          // Add more labels/groups here
+        ],
+      },
+      {
+        group: "Group 2",
+        data: [
+          {
+            label: "Data Point 3",
+            data: [
+              {
+                timeRange: [new Date('2023-07-12'), new Date('2023-07-20')],
+                val: 12
+              },
+              // Add more data points for 'Data Point 3'
+            ]
+          },
+          // Add more data points for 'Group 2'
+        ]
+      },
+      // Add more groups here
+    ];
+    
+  
+    
+  }
+
+  
+
+
 
   
   }
